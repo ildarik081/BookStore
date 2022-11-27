89e0f903fbcc02531f963d8177b55f64fc6a7e02
ildarik081 <61247445+ildarik081@users.noreply.github.com>
2022-11-27 13:29:29 +0400

    delete vendor

--- vendor/gam6itko/ozon-seller/src/Exception/AccessDeniedException.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-namespace Gam6itko\OzonSeller\Exception;
-
-class AccessDeniedException extends OzonSellerException
-{
-}
--- vendor/gam6itko/ozon-seller/src/Exception/InternalException.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-namespace Gam6itko\OzonSeller\Exception;
-
-class InternalException extends OzonSellerException
-{
-}
