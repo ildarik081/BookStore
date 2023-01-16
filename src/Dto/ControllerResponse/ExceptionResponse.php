3838f8bd373a87cf1a2aeb74bcfe6185b5eca7a0
ildarik081 <ildarik081@gmail.com>
2023-01-16 19:07:02 +0400

    add trace

--- src/Dto/ControllerResponse/ExceptionResponse.php
+++ src/Dto/ControllerResponse/ExceptionResponse.php
@@ -10,12 +10,21 @@ class ExceptionResponse implements ControllerResponseInterface
     /**
      * @var string
      * @Annotation\Type("string")
+     * @Annotation\SerializedName("code")
      */
     public string $code;
 
     /**
      * @var string
      * @Annotation\Type("string")
+     * @Annotation\SerializedName("message")
      */
     public string $message;
+
+    /**
+     * @var array
+     * @Annotation\Type("array")
+     * @Annotation\SerializedName("trace")
+     */
+    public array $trace;
 }
